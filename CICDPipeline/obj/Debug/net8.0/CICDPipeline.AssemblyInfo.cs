6a09a102559d0e83ae0d7274076ed657a984d8c6
Jenna Hamarus <129057835+JennaHamarus@users.noreply.github.com>
2025-02-19 11:29:19 +0200

    Azure

--- CICDPipeline/obj/Debug/net8.0/CICDPipeline.AssemblyInfo.cs
+++ CICDPipeline/obj/Debug/net8.0/CICDPipeline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICDPipeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1b9c44ffd4c2ef3ad148abd9b1fe317313a719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a5f90b8959452626be6233c49201cba8e8f200")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICDPipeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICDPipeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
